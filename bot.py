365a9cd784eefd53713da391e007f33b24212a9a
Navid <51940962+Navid-d@users.noreply.github.com>
2020-10-19 12:40:10 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -222,6 +222,9 @@ def send_season_one(message):
             elif m[1] == '15':
                 bot.send_message(message.chat.id, m[1])
                 bot.forward_message(message.chat.id, -1001410450666, 16)
+            elif m[1] == '16':
+                bot.send_message(message.chat.id, m[1])
+                bot.forward_message(message.chat.id, -1001410450666, 17)
             else:
                 print("Error")
     except:
