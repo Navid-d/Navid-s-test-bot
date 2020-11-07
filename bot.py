a75403a6d197208a3501bc9a6fccd9bce800f19a
Navid <51940962+Navid-d@users.noreply.github.com>
2020-11-08 00:14:01 +0430

    Update bot.py

--- bot.py
+++ bot.py
@@ -334,7 +334,7 @@ def zeyghami_pnb(message):
 @bot.message_handler(commands=["kill", "vote"])
 def kill(message):
     try:
-        bot.send_message(message.chat.id, f"{message.text.split( )[1]} {choice(['was', 'was not'])} an impostor")
+        bot.send_message(message.chat.id, f"{message.text.split(" ")[1:]} {choice(['was', 'was not'])} an impostor")
     except:
         pass
 
